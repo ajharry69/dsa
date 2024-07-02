64156714bacfbc645519dde3ffe442e8b223459d
Orinda Harrison (Mitch) <mitchke@duck.com>
2024-07-02 15:25:28 +0300

    Changes I have forgot about

---
+++ jambo/mixins.py
@@ -0,0 +1,14 @@
+from rest_framework.response import Response
+
+
+class PaginatedResponseModelViewSetMixin:
+    def paginate_response(self, queryset):
+        queryset = self.filter_queryset(queryset)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
