3d73fd958e69d869a272c848f9d2a4e402b9e1ab
Orinda Harrison (Mitch) <mitchke@duck.com>
2024-07-16 10:09:51 +0300

    Print path matrix

--- pipesystem/__init__.py
+++ pipesystem/__init__.py
@@ -9,6 +9,7 @@ def connected_sinks(filepath):
         A string of uppercase letters representing the connected sinks in alphabetical order.
     """
     grid = {}
+    sinks = set()
     x_max = y_max = 0
 
     # Read pipe system data
@@ -17,6 +18,8 @@ def connected_sinks(filepath):
             obj, x, y = line.strip().split()
             x, y = int(x), int(y)
             grid[(x, y)] = obj
+            if obj.isalpha():
+                sinks.add(obj)
 
             x_max = max(x_max, x)
             y_max = max(y_max, y)
@@ -33,18 +36,54 @@ def connected_sinks(filepath):
         '╦': {'R', 'L', 'B'},
         '╩': {'R', 'L', 'T'},
     }
-    y_count = y_max
-    while y_count >= 0:
-        data = ""
-        for x in range(x_max + 1):
-            d = grid.get((x, y_count), " ")
-            data += d
-        print(data)
-        y_count -= 1
+
+    matrix = seq_matrix(grid, x_max, y_max)
+
+    print("=" * 80)
+
+    reverse_seq_matrix(grid, matrix, x_max, y_max)
     return ""
 
 
+def seq_matrix(grid, x_max, y_max):
+    y = y_max
+    while y >= 0:
+        yc = (y_max - y)
+        data = ""
+        for x in range(x_max + 1):
+            data += grid.get((x, yc), " ")
+        print(data)
+        y -= 1
+    matrix = []
+    y = y_max
+    while y >= 0:
+        yc = (y_max - y)
+        matrix_row = []
+        for x in range(x_max + 1):
+            matrix_row.append((x, yc, grid.get((x, yc), " ")))
+        matrix.append(matrix_row)
+        print(matrix_row)
+        y -= 1
+    return matrix
+
+
+def reverse_seq_matrix(grid, matrix, x_max, y_max):
+    y = y_max
+    while y >= 0:
+        data = ""
+        for x in range(x_max + 1):
+            data += grid.get((x, y), " ")
+        print(data)
+        y -= 1
+    y = y_max
+    while y >= 0:
+        print(matrix[y])
+        y -= 1
+
+
 if __name__ == '__main__':
-    # for path in ("input1.txt", "input2.txt"):
-    for path in ("input1.txt", "input1d.txt",):
+    for path in (
+            "input1.txt",
+            # "input2.txt",
+    ):
         print(f"Connected sinks for '{path}':", connected_sinks(filepath=path))
