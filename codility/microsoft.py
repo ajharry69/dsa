70b1612473e9285f88743518ab01fcc3d70792cc
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-01-03 15:59:53 +0300

    Microsoft codility solution

--- codility/microsoft.py
+++ codility/microsoft.py
@@ -1,2 +1,47 @@
-def solution(a, b):
-    return a + b
+def solution1(values, K, L):
+    values.sort()
+    n = len(values)
+    for i in range(L, n):
+        if values[i] > K or values[i - L] == values[i] - L:
+            return False
+    return values[n - 1] == K
+
+
+# def solution1(values, K, L):
+#     counts = {}
+#     for value in values:
+#         counts[value] = counts.get(value, 0) + 1
+#         if value > K or counts[value] > L:
+#             return False
+#     return True
+
+def solution2(A, B):
+    left, right = 1, max(A, B)
+    result = 0
+
+    while left <= right:
+        mid = (left + right) // 2
+        if (A // mid) + (B // mid) >= 4:
+            result = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return result
+
+
+def solution3(A, B):
+    gc = int(len(A) / 2)
+    sc = int(len(B) / 2)
+
+    ra = list(set(A))[:gc]
+
+    rb = []
+    for n in set(B):
+        if len(rb) == sc:
+            break
+
+        if n not in ra:
+            rb.append(n)
+
+    return  len(ra + rb)
--- tests/codility/test_microsoft.py
+++ tests/codility/test_microsoft.py
@@ -1,5 +1,38 @@
-from codility.microsoft import solution
+from codility.microsoft import solution1, solution2, solution3
 
 
-def test_solution():
-    assert solution(1, 2) == 3
+def test_solution1():
+    assert solution1([1, 1, 4, 4], 4, 2) == True
+    assert solution1([1, 1, 2, 4], 4, 1) == False
+    assert solution1([4, 2, 5], 5, 2) == True
+    assert solution1([4, 2, 2, 2, 5], 5, 2) == False
+
+
+def test_solution2():
+    """
+    m = 5
+    l = 6
+    r = 10
+    ---------
+    m = 8
+    l = 6
+    r = 10
+    ---------
+    m = 6
+    l = 7
+    r = 7
+    ---------
+    m = 7
+    l = 8
+    r = 7
+    ---------
+    """
+    assert solution2(A=10, B=21) == 7
+    assert solution2(A=13, B=11) == 5
+    assert solution2(A=2, B=1) == 0
+    assert solution2(A=1, B=8) == 2
+
+
+def test_solution3():
+    assert solution3([1, 2, 3, 4], [3, 3, 3, 7]) == 4
+    assert solution3([2, 2, 2, 2, 2, 2], [7, 4, 2, 5, 1, 2]) == 4
