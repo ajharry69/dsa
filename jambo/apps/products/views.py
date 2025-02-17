a7ba7c67e16a782f194cf907ced5f576228f0691
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-02-17 17:34:54 +0300

    [Approach 2] - get products purchasable within budget

--- jambo/apps/products/views.py
+++ jambo/apps/products/views.py
@@ -1,61 +1,6 @@
-from django.db.models import F
 from django.http import JsonResponse
 
 from jambo.apps.products.models import Product
-
-
-def _products_purchasable_within_budget(budget, products):
-    purchasable_products = {}
-    other_purchasable_products = {}
-
-    remaining_budget = budget
-
-    maximum_purchasable_product_name = ""
-
-    for (name, price, quantity, total_price) in products:
-        if total_price <= remaining_budget:
-            purchasable_products[name] = quantity
-            remaining_budget -= total_price
-
-            if remaining_budget == 0:
-                break
-        else:
-            # here we are asking ourselves how many X dollar products can we buy
-            # with the remaining budget?
-            purchasable_quantity = min(int(remaining_budget // price), quantity)
-
-            if purchasable_quantity == 0:
-                continue
-
-            other_purchasable_products[name] = (purchasable_quantity, price)
-
-            if not maximum_purchasable_product_name or purchasable_quantity > \
-                    other_purchasable_products[maximum_purchasable_product_name][0]:
-                maximum_purchasable_product_name = name
-
-    while len(other_purchasable_products) > 0 and remaining_budget > 0:
-        quantity, price = other_purchasable_products.pop(maximum_purchasable_product_name)
-        purchasable_products[maximum_purchasable_product_name] = quantity
-        remaining_budget -= (quantity * price)
-
-        for name, quantity_price in other_purchasable_products.copy().items():
-            quantity, price = quantity_price
-            purchasable_quantity = min(int(remaining_budget // price), quantity)
-
-            if purchasable_quantity == 0:
-                del other_purchasable_products[name]
-                continue
-
-            other_purchasable_products[name] = (purchasable_quantity, price)
-
-            if not maximum_purchasable_product_name or purchasable_quantity > \
-                    other_purchasable_products[maximum_purchasable_product_name][0]:
-                maximum_purchasable_product_name = name
-
-    return {
-        "remaining_budget": remaining_budget,
-        "purchasable_products": purchasable_products,
-    }
 
 
 def products_purchasable_within_budget(request):
@@ -70,13 +15,38 @@ def products_purchasable_within_budget(request):
     except (ValueError, KeyError) as _:
         return JsonResponse(status=404, data={"error": "Service requires a 'budget' as a query parameter"})
 
-    products = (
-        Product.objects.annotate(total_price=F("price") * F("quantity"))
-        .order_by("quantity", "-price")
-        .values_list("name", "price", "quantity", "total_price")
+    remaining_budget = budget
+    maximum_purchasable_product_name = ""
+
+    purchasable_products = {}
+    products = {
+        product.name: (product.quantity, product.price)
+        for product in Product.objects.all()
+    }
+
+    while len(products) > 0 and remaining_budget > 0:
+        if maximum_purchasable_product_name:
+            quantity, price = products.pop(maximum_purchasable_product_name)
+            purchasable_products[maximum_purchasable_product_name] = quantity
+            remaining_budget -= (quantity * price)
+
+        for name, quantity_price in products.copy().items():
+            quantity, price = quantity_price
+            purchasable_quantity = min(int(remaining_budget // price), quantity)
+
+            if purchasable_quantity == 0:
+                del products[name]
+                continue
+
+            products[name] = (purchasable_quantity, price)
+
+            if not maximum_purchasable_product_name or purchasable_quantity > \
+                    products[maximum_purchasable_product_name][0]:
+                maximum_purchasable_product_name = name
+
+    return JsonResponse(
+        data={
+            "remaining_budget": remaining_budget,
+            "purchasable_products": purchasable_products,
+        },
     )
-    data = _products_purchasable_within_budget(
-        budget=budget,
-        products=products,
-    )
-    return JsonResponse(data=data)
